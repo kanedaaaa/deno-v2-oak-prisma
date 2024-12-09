aeee21c5e510d8316c4e0e1e74d12b6857123835
kanedaaaa <samflynn19121@gmail.com>
2024-12-09 04:42:56 +0400

    chore/ remove dev comments

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -8,18 +8,11 @@ class CustomError extends Error {
     clientMessage: string = "Something Went Wrong",
     statusCode: number = 500,
   ) {
-    // Use clientMessage as the base error message if serverMessage is not a string
     super(serverMessage || clientMessage);
-
-    // Ensure type safety
     this.serverMessage = serverMessage;
     this.clientMessage = clientMessage;
     this.statusCode = statusCode;
-
-    // Maintain prototype chain for proper instanceof checks
     Object.setPrototypeOf(this, CustomError.prototype);
-
-    // Capture stack trace
     Error.captureStackTrace(this, this.constructor);
   }
 }
